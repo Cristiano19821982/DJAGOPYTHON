1655d55339e0173ed0c6ebc34db37cb6ff5f722e
Cristiano <cfp.cristiano@hotmail.com>
2023-03-03 11:03:26 -0300

    app

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('HOME3')
+    return render('recipes/template/home.html')
 
 
 def contato(request):
